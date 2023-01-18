a9f592fbe1352583bcc3a65ff7dc4d65a5e929d8
Matt Iannucci <matt.iannucci@rpsgroup.com>
2023-01-18 11:45:32 -0500

    Wrokign map with tailwind

--- glo-eastern-region-viz/src/App.svelte
+++ glo-eastern-region-viz/src/App.svelte
@@ -1,7 +1,33 @@
 <script lang="ts">
+  import "mapbox-gl/src/css/mapbox-gl.css";
+  import mapbox from "mapbox-gl";
+  import { onMount } from "svelte";
 
+  const MAPBOX_ACCESS_TOKEN =
+    "pk.eyJ1Ijoib3BlbnNvdXJjZXJlciIsImEiOiJja3lsbzNveHAwbndkMnZwZXYxeWxnM3pzIn0.BD4akCoe1u4dg7gcl3J4cQ";
+  mapbox.accessToken = MAPBOX_ACCESS_TOKEN;
+
+  let map: mapbox.Map | undefined = undefined;
+
+  onMount(async () => {
+    map = new mapbox.Map({
+      container: "map",
+      style: "mapbox://styles/mapbox/light-v10",
+      center: [-94.4, 30],
+      zoom: 8,
+    });
+    map.on("load", () => {
+      map.resize();
+    });
+  });
 </script>
 
-<main>
-  <h1>Hello World</h1>
-</main>
+<div class="h-screen w-screen flex flex-col">
+  <nav class="flex flex-row px-8 py-2">
+    <h1 class="font-bold text-xl">GLO Eastern Region Visualizer</h1>
+  </nav>
+
+  <main class="flex-1 flex-col w-full h-full bg-red">
+    <div id="map" class="h-full w-full" />
+  </main>
+</div>
